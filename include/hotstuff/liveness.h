63c8b43af7599c716457b03a15e21f40664b43e4
Tomas Pereira <tomas.maria.pereira@hotmail.com>
2024-02-21 02:35:27 +0000

    Oh well

--- include/hotstuff/liveness.h
+++ include/hotstuff/liveness.h
@@ -357,7 +357,6 @@ public:
         // forced_reconfig_timer.del()
         // forced_reconfig_timer = TimerEvent(ec, salticidae::generic_bind(&PaceMakerDummyFixedTwo::set_proposer_forced, this, _1));
         // forced_reconfig_timer.add(forced_timeout);
-        schedule_next();
     }
 
     void unlock(TimerEvent &) {
