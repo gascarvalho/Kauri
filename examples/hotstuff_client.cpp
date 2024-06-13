8c28693923d646d36fda3b745ddeea0fa6faa7ed
Tomas Pereira <tomas.maria.pereira@hotmail.com>
2024-06-13 21:20:45 +0000

    a

--- examples/hotstuff_client.cpp
+++ examples/hotstuff_client.cpp
@@ -66,8 +66,7 @@ std::vector<std::pair<struct timeval, double>> elapsed;
 Net mn(ec, Net::Config());
 
 void connect_all() {
-    for (size_t i = 0; i < replicas.size(); i++)
-            conns.insert(std::make_pair(i, mn.connect_sync(replicas[i])));
+    conns.insert(std::make_pair(i, mn.connect_sync(replicas[0])));
 }
 
 bool try_send(bool check = true) {
