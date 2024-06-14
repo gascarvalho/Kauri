68d4bebc94040fc0d9edaa370bc1fbfdacc4b254
Tomas Pereira <tomas.maria.pereira@hotmail.com>
2024-06-14 18:48:41 +0000

    Test

--- examples/hotstuff_client.cpp
+++ examples/hotstuff_client.cpp
@@ -66,7 +66,8 @@ std::vector<std::pair<struct timeval, double>> elapsed;
 Net mn(ec, Net::Config());
 
 void connect_all() {
-    conns.insert(std::make_pair(0, mn.connect_sync(replicas[0])));
+    for (size_t i = 0; i < replicas.size(); i++)
+        conns.insert(std::make_pair(i, mn.connect_sync(replicas[i])));
 }
 
 bool try_send(bool check = true) {
--- runkauri/server.sh
+++ runkauri/server.sh
@@ -17,7 +17,7 @@ service1="server1-$KAURI_UUID"
 
 # Make sure correct branch is selected for crypto
 cd MSc-Kauri && git pull && git submodule update --recursive --remote
-git checkout multi-tree
+git checkout deploy-test
 
 # Do a quick compile of the branch
 git pull && cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED=ON -DHOTSTUFF_PROTO_LOG=ON && make
