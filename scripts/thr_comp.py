43d2c540727a81048fa0aa9e1db01cad6d330de1
Tomas Pereira <tomas.maria.pereira@hotmail.com>
2024-06-26 11:39:35 +0100

    Revamp

--- scripts/thr_comp.py
+++ scripts/thr_comp.py
@@ -1,10 +1,12 @@
 import sys
 import re
 import argparse
+import math
 import numpy as np
 import matplotlib.pyplot as plt
 from datetime import datetime, timedelta
 from scipy.interpolate import interp1d
+import matplotlib.colors as mcolors
 
 def remove_outliers(x, outlierConstant=1.5):
     a = np.array(x)
@@ -26,103 +28,39 @@ def str2datetime(s):
     dt = datetime.strptime(parts[0], "%Y-%m-%d %H:%M:%S")
     return dt.replace(microsecond=int(parts[1]))
 
-def plot_hist(fnames, labels, window_size, blksize, cutoff):
-    plt.rcParams["figure.figsize"] = (6, 3.9)
-    
+def plot_hist(fname, data, labels, window_size, blksize):
+    plt.rcParams["figure.figsize"] = (10, 6)
     plt.xlabel(r"tempo (s)")
-    plt.ylabel(r"tx" if args.blksize else r"blocos")
+    
+    if args.blksize is None:
+        plt.ylabel(r"blocos")
+    else:
+        plt.ylabel(r"tx")
 
-    colors = plt.cm.get_cmap('tab10').colors  # Get a colormap with 10 colors
-    color_idx = 0
-
-    for fname, label in zip(fnames, labels):
-        timestamps, rcf_timestamps = parse_file(fname)
-        begin_time, values, reconfig_x, total, total_time, avg_tx_sec = process_data(timestamps, rcf_timestamps, window_size, blksize, cutoff)
-
-        x = [i * window_size for i in range(len(values))]
-        color = colors[color_idx % len(colors)]
-        color_idx += 1
-
-        plt.plot(x, values, marker='o', markersize=3.5, label=label, color=color)
-
-        # Reconfiguration markers
-        interp_func = interp1d(x, values, kind='linear', fill_value='extrapolate')
+    colors = list(mcolors.TABLEAU_COLORS)
+    for i, (x, y, reconfig_x, avg_tx_sec, total, total_time) in enumerate(data):
+        color = colors[i]
+        label = labels[i] if labels else f'Plot {i+1}'
+        
+        plt.plot(x, y, marker='o', markersize=3.5, label=label, color=color)
+        
+        interp_func = interp1d(x, y, kind='linear', fill_value='extrapolate')
         reconfig_y = interp_func(reconfig_x)
         plt.plot(reconfig_x, reconfig_y, 'D', color=color)
-
-        # Average transaction per second for whole experiment
+        
         plt.axhline(y=(total / total_time), color=color, linestyle='-', linewidth=1)
-        plt.text(0, avg_tx_sec, 'Média=' + str(round(float(avg_tx_sec), 3)), color=color, ha='left', va='bottom', bbox=dict(facecolor='white', alpha=0.75), fontsize=10)
+        plt.text(-20, avg_tx_sec, 'Média=' + str(round(float(avg_tx_sec), 3)), color=color, ha='left', va='bottom', bbox=dict(facecolor='white', alpha=0.75), fontsize=10)
 
+    plt.xlim(left=0)
+    plt.ylim(bottom=0)
     plt.legend()
-    plt.savefig("hist.png")
+    plt.savefig(fname)
     plt.show()
 
-def parse_file(fname):
-    commit_pat = re.compile('([^[].*) \[hotstuff proto\] commit (.*)')
-    reconfig_pat = re.compile('([^[].*) \[hotstuff proto\] \[PMAKER\] Timeout reached!!!')
-
-    timestamps = []
-    rcf_timestamps = []
-
-    with open(fname, 'r') as file:
-        lines = file.readlines()
-        for line in lines:
-            m = commit_pat.match(line)
-            rcf = reconfig_pat.match(line)
-            if m:
-                timestamps.append(str2datetime(m.group(1)))
-            elif rcf:
-                rcf_timestamps.append(str2datetime(rcf.group(1)))
-    
-    timestamps.sort()
-    rcf_timestamps.sort()
-
-    return timestamps, rcf_timestamps
-
-def process_data(timestamps, rcf_timestamps, window_size, blksize, cutoff):
-    begin_time = None
-    cnt = 0
-    total = 0
-    values = []
-    reconfig_x = []
-
-    cutoff_time = None
-    for timestamp in timestamps:
-        if begin_time is None:
-            begin_time = timestamp
-            cutoff_time = begin_time + timedelta(seconds=cutoff)
-
-        if timestamp > cutoff_time:
-            break
-
-        while timestamp >= begin_time + timedelta(seconds=window_size):
-            elapsed_time = (begin_time + timedelta(seconds=window_size) - begin_time).total_seconds()
-            values.append(cnt / elapsed_time)
-            begin_time += timedelta(seconds=window_size)
-            cnt = 0
-        cnt += blksize
-        total += blksize
-
-    elapsed_time = (begin_time + timedelta(seconds=window_size) - begin_time).total_seconds()
-    values.append(cnt / elapsed_time)
-
-    start_time = timestamps[0]
-    end_time = timestamps[-1]
-
-    if end_time > cutoff_time:
-        end_time = cutoff_time
-
-    total_time = (end_time - start_time).total_seconds()
-    avg_tx_sec = total / total_time
-
-    for rcf_timestamp in rcf_timestamps:
-        if rcf_timestamp > cutoff_time:
-            break
-        elapsed_time = (rcf_timestamp - start_time).total_seconds()
-        reconfig_x.append(elapsed_time)
-
-    return begin_time, values, reconfig_x, total, total_time, avg_tx_sec
+def find_nearest(array, value):
+    array = np.asarray(array)
+    idx = (np.abs(array - value)).argmin()
+    return array[idx]
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -130,14 +68,80 @@ if __name__ == '__main__':
     parser.add_argument('--blksize', type=float, required=False)
     parser.add_argument('--output', type=str, default="hist.png", required=False)
     parser.add_argument('--files', type=str, nargs='+', required=True)
-    parser.add_argument('--labels', type=str, nargs='+', required=True)
+    parser.add_argument('--labels', type=str, nargs='+', required=False)
     parser.add_argument('--cutoff', type=float, default=9999, required=False)
     args = parser.parse_args()
+    commit_pat = re.compile('([^[].*) \[hotstuff proto\] Core deliver')
+    reconfig_pat = re.compile('([^[].*) \[hotstuff proto\] \[PMAKER\] Timeout reached!!!')
+    
+    window_size = args.window_size
+    blksize = args.blksize
+    data = []
 
-    if len(args.files) != len(args.labels):
-        print("The number of files must match the number of labels.")
-        sys.exit(1)
+    if blksize is None:
+        blksize = 1
 
-    blksize = args.blksize if args.blksize else 1
-    plot_hist(args.files, args.labels, args.window_size, blksize, args.cutoff)
+    for file in args.files:
+        timestamps = []
+        rcf_timestamps = []
+        values = []
+        x = []
 
+        with open(file, 'r') as f:
+            lines = f.readlines()
+            for line in lines:
+                m = commit_pat.match(line)
+                rcf = reconfig_pat.match(line)
+                if m:
+                    timestamps.append(str2datetime(m.group(1)))
+                elif rcf:
+                    rcf_timestamps.append(str2datetime(rcf.group(1)))
+                
+        timestamps.sort()
+        rcf_timestamps.sort()
+
+        start_time = timestamps[0]
+        end_time = timestamps[-1]
+
+        begin_time = None
+        cnt = 0
+        total = 0
+        cutoff_time = None
+        values = []
+        x = []
+
+        for timestamp in timestamps:
+            if begin_time is None:
+                begin_time = timestamp
+                cutoff_time = begin_time + timedelta(seconds=args.cutoff)
+
+            if timestamp > cutoff_time:
+                break
+
+            while timestamp >= begin_time + timedelta(seconds=window_size):
+                x += [(begin_time - start_time).total_seconds()]
+                values.append(cnt / window_size)
+                begin_time += timedelta(seconds=window_size)
+                cnt = 0
+            cnt += blksize
+            total += blksize
+
+        values.append(cnt / window_size)
+        x += [(begin_time - start_time).total_seconds()]
+
+        if end_time > cutoff_time:
+            end_time = cutoff_time
+
+        total_time = (end_time - start_time).total_seconds()
+        avg_tx_sec = total / total_time
+
+        reconfig_x = []
+        for rcf_timestamp in rcf_timestamps:
+            if rcf_timestamp > cutoff_time:
+                break
+            elapsed_time = (rcf_timestamp - start_time).total_seconds()
+            reconfig_x.append(find_nearest(x, elapsed_time))
+
+        data.append((x, values, reconfig_x, avg_tx_sec, total, total_time))
+
+    plot_hist(args.output, data, args.labels, window_size, blksize)
