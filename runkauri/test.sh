c8c92efdac1b3f1a76e0e28094137aae44070e81
gascarvalho <goncalo.s.carvalho@tecnico.ulisboa.pt>
2024-09-17 17:34:03 +0100

    Adds client service to the swarm (client aka reputation server)

--- runkauri/test.sh
+++ runkauri/test.sh
@@ -15,10 +15,14 @@ FLINES=$(cat $FILENAME2 | grep "^[^#;]")
 LOG_FOLDER="logs" # Define a folder to store logs
 REPLICA_LOG_FOLDER="${LOG_FOLDER}/replicas"
 CLIENT_LOG_FOLDER="${LOG_FOLDER}/clients"
+
+#Cleans previous logs (from servers and clients)
 rm -f $REPLICA_LOG_FOLDER/*
-mkdir -p $LOG_FOLDER # Create the log folder if it doesn't exist
+rm -f $CLIENT_LOG_FOLDER/*
+
+mkdir -p $LOG_FOLDER         # Create the log folder if it doesn't exist
 mkdir -p $REPLICA_LOG_FOLDER # Subfolder for replica logs
-mkdir -p $CLIENT_LOG_FOLDER # Subfolder for client logs
+mkdir -p $CLIENT_LOG_FOLDER  # Subfolder for client logs
 
 # Function to log messages
 logger() {
@@ -26,15 +30,14 @@ logger() {
 }
 
 # Each LINE in the experiment file is one experimental setup
-for LINE in $FLINES
-do
+for LINE in $FLINES; do
   echo '---------------------------------------------------------------'
   echo $LINE
-  IFS=':' read -ra split <<< "$LINE"
+  IFS=':' read -ra split <<<"$LINE"
 
-  sed  "s/${COMMAND_STRING}/${split[0]}/g" $FILENAME > $EXPORT_FILENAME
-  sed  -i "s/${QTY1_STRING}/${split[1]}/g" $EXPORT_FILENAME
-  sed  -i "s/${QTY2_STRING}/${split[2]}/g" $EXPORT_FILENAME
+  sed "s/${COMMAND_STRING}/${split[0]}/g" $FILENAME >$EXPORT_FILENAME
+  sed -i "s/${QTY1_STRING}/${split[1]}/g" $EXPORT_FILENAME
+  sed -i "s/${QTY2_STRING}/${split[2]}/g" $EXPORT_FILENAME
 
   echo '**********************************************'
   echo "*** This setup needs ${split[3]} physical machines! ***"
@@ -42,50 +45,36 @@ do
 
   #the command == data between [] in the test inputs
 
-  for i in {1..1}
-  do
+  for i in {1..1}; do
     TIMESTAMP=$(date +%F_%T)
 
     # Deploy experiment
     docker stack deploy -c kauri-temp.yaml kauriservice &
-     
+
     # Docker startup time 100s + 1*60s of experiment runtime
     sleep 300
 
     replica_index=0
     # Collect and print results.
-    for container in $(docker ps -q -f name="server")
-    do
-      replica_index=$((replica_index+1))
+    for container in $(docker ps -q -f name="server"); do
 
-      if [ ! $(docker exec -it $container bash -c "cd MSc-Kauri && test -e log*") ]
-      then
+      echo "Gathering logs from container $container..."
+
+      replica_index=$((replica_index + 1))
+
+      if [ ! $(docker exec -it $container bash -c "cd MSc-Kauri && test -e log*") ]; then
         REPLICA_LOG_FILE="${REPLICA_LOG_FOLDER}/log_${TIMESTAMP}_${replica_index}.txt"
-        docker exec -it $container bash -c "cd MSc-Kauri && cat log*" > $REPLICA_LOG_FILE
-      fi
-
-      if [ ! $(docker exec -it $container bash -c "cd MSc-Kauri && test -e clientlog*") ]
-      then
-        CLIENT_LOG_FILE="${CLIENT_LOG_FOLDER}/clientlog_${TIMESTAMP}_${replica_index}.txt"
-        docker exec -it $container bash -c "cd MSc-Kauri && cat clientlog*" > $CLIENT_LOG_FILE
+        docker exec -it $container bash -c "cd MSc-Kauri && cat log*" >$REPLICA_LOG_FILE
       fi
 
     done
 
-    for container in $(docker ps -q -f name="client")
-    do
+    for container in $(docker ps -q -f name="client"); do
       #add iterator
 
-      if [ ! $(docker exec -it $container bash -c "cd MSc-Kauri && test -e log*") ]
-      then
-        REPLICA_LOG_FILE="${REPLICA_LOG_FOLDER}/log_${TIMESTAMP}_999.txt"
-        docker exec -it $container bash -c "cd MSc-Kauri && cat log*" > $REPLICA_LOG_FILE
-      fi
-
-      if [ ! $(docker exec -it $container bash -c "cd MSc-Kauri && test -e clientlog*") ]
-      then
+      if [ ! $(docker exec -it $container bash -c "cd MSc-Kauri && test -e clientlog*") ]; then
         CLIENT_LOG_FILE="${CLIENT_LOG_FOLDER}/clientlog_${TIMESTAMP}_999.txt"
-        docker exec -it $container bash -c "cd MSc-Kauri && cat clientlog*" > $CLIENT_LOG_FILE
+        docker exec -it $container bash -c "cd MSc-Kauri && cat clientlog*" >$CLIENT_LOG_FILE
       fi
 
     done
@@ -100,7 +89,7 @@ do
       fi
     done < <(find "$REPLICA_LOG_FOLDER" -regextype posix-egrep -regex '.*_[0-9]+\.txt')
 
-    # Rename client log files to have correct replica name
+    #Rename client log files to have correct replica name
     while IFS= read -r log_file; do
       replica_index=$(grep -o "I am client with id = [0-9]\+" "$log_file" | grep -o '[0-9]\+' | tail -n 1)
       if [ -n "$replica_index" ]; then
